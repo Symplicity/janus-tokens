0abd6daa874cf8e42f798cd29263f094ccdd7a3d
Christian Boyle <mail@christianboyle.com>
2021-04-09 10:28:05 -0500

    Fix janus-badge-font-size token value (.75em->.75rem)

--- dist/type.common.js
+++ dist/type.common.js
@@ -14,5 +14,5 @@ module.exports = {
   janusCardTitleLineHeight: "1.25rem",
   janusAvatarFontSize: "1vw",
   janusAvatarLineHeight: "1rem",
-  janusBadgeFontSize: ".75em",
+  janusBadgeFontSize: ".75rem",
 };
