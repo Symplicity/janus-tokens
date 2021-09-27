c3955957c455450a2e0e01296c153ca65d779269
Christian Boyle <mail@christianboyle.com>
2021-09-27 11:16:50 -0500

    Update value for janus-badge-padding-y and janus-badge-padding-x to match spec (20px height, padding: 3px 8px)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,8 +6,8 @@ declare interface Tokens {
   janusAvatarFontSize: "1vw";
   janusAvatarLineHeight: "1rem";
   janusBadgeFontSize: ".75em";
-  janusBadgePaddingX: ".6em";
-  janusBadgePaddingY: ".3em";
+  janusBadgePaddingX: ".667em";
+  janusBadgePaddingY: ".25em";
   janusBorderRadius: ".25rem";
   janusBorderRadiusLg: ".188rem";
   janusBorderRadiusSm: ".125rem";
